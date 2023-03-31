016db5c90ef18a3eb4adde5d07ac293a8ffed64a
eze-latech <ezequiel@latechfactory.com>
2023-03-31 13:19:44 -0300

    [DEV]: Server changes & refactors

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,16 +1,11 @@
-import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.PROJECT_DB_USERNAME!,
-  process.env.PROJECT_DB_PASSWORD!,
-  {
-    host: "localhost",
-    dialect: "mysql",
-  }
-);
+const db = new Sequelize(process.env.DB_NAME!, process.env.PROJECT_DB_USERNAME!, process.env.PROJECT_DB_PASSWORD!, {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 export default db;
